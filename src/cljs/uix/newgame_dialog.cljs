1d615f2fe9999078f3b2452a0ee20cebdd81862e
BenHammond <ben@vorpalsystems.co.uk>
2020-04-26 14:54:36 +0100

    this seems to work adequately

--- src/cljs/uix/newgame_dialog.cljs
+++ src/cljs/uix/newgame_dialog.cljs
@@ -9,6 +9,7 @@
             ["@material-ui/core/Button" :as button]
             ["@material-ui/core/FormControlLabel" :as form-controllabel]
             ["@material-ui/core/FormGroup" :as form-group]
+            ["@material-ui/core/FormLabel" :as form-label]
             ["@material-ui/core/IconButton" :as icon-button]
             ["@material-ui/core/MenuItem" :as menu-item]
             ["@material-ui/core/Menu" :as menu]
@@ -25,7 +26,8 @@
             [uix.core.alpha :as uix]
             [uix.compiler.aot :as compiler.aot]
             [uix.compiler.alpha :as compiler]
-            [xframe.core.alpha :as xf]))
+            [xframe.core.alpha :as xf]
+            [clojure.string :as string]))
 
 (def ^:const dbk :uix/newgame-dialog)
 
@@ -40,96 +42,69 @@
 (xf/reg-sub ::visible
   (fn [] (xf/<- [[::xf/db] dbk])))
 
-(def playerswitch-config
-  {true {:player :player/red
+(def player-config
+  {true {:domain :player/red
          :label "RED"}
-   false {:player :player/black
-          :label "BLACK"}})
+   false {:domain :player/black
+          :label "BLACK"}
+   :default-key true})
+
+(defn new-switchstate
+  [f v]
+  (.log js/console "v is " v)
+  (.log js/console "fv is " (f v))
+  (let [m2 (f v)
+        bb (boolean v)
+        m3 (assoc m2 :checked bb)]
+    m3))
+
+(defn default-switchstate
+  [f]
+  (->> (f :default-key)
+    (new-switchstate f)))
 
 (defn switchgroup
-  [{:keys [state* vals-fn]}]
-  (.log js/console "switchgroup: state* is " @state*)
-  (let [label-val* (uix/cursor-in state* [:label])
-        checked-val* (uix/cursor-in state* [:checked])
-        domain-val* (uix/cursor-in state* [:domain])
+  [{:keys [title vals-fn output]}]
+  (let [mystate* (uix/state (default-switchstate vals-fn))
         change-fn (fn [evt]
-                    (.log js/console "switchgroup-changefn" evt)
-                    (.log js/console "switchgroup-changefn evt-taraget-chkesked" js/evt.target.checked)
-                    (.log js/console "switchgroup-change to" (vals-fn js/evt.target.checked))
-
-                    (let [{:keys [label domain]}
-                          (vals-fn js/evt.target.checked)]
-                      (reset! label-val* label)
-                      (reset! checked-val* js/evt.target.checked)
-                      (reset! domain-val* domain)
-                      (.log js/console "changefn: state* AFTER is " @state*)))]
-    (uix/effect!
-      (fn [] (when (nil? @state*)
-               (.log js/console "switchgroup - setting default!")
-               (reset! state* (merge {:checked false} (vals-fn :default)))))
-      [])
-
-    #_(when (nil? @label-val*)
-        (reset! label-val* (:label (vals-fn :default))))
-    #_(when (nil? @checked-val*)
-      (reset! checked-val* (:checked (vals-fn :default))))
+                    (let [chk (boolean js/evt.target.checked)]
+                      (let [newstate (new-switchstate vals-fn chk)]
+                        (reset! output (:domain newstate))
+                        (reset! mystate* newstate))))]
     [:> form-group/default {:row true}
+     (when-not (string/blank? title)
+       [:> form-label/default {:component "legend"} title])
      [:> form-controllabel/default
-      {:label @label-val*
+      {:label (:label @mystate*)
        :control
        (uix/as-element
          [:> switch/default
-          {:checked @checked-val*
+          {:checked (:checked @mystate*)
            :onChange change-fn}])}]
      ]))
 
-(def inviteonly-statevals
-  {false {:label "Oppenent coube be anyone"
+(def invitation-config
+  {false {:label "Anyone"
           :domain :opponent/anyone}
-   true {:label "Opponent must be invited"
+   true {:label "Invite Only"
          :domain :opponent/invite-only}
-   :default {:label "Oppenent coube be anyone"
-             :domain :opponent/anyone}})
-
-(defn defaulting-cursor-in
-  [ref path default]
-  (let [r (uix/cursor-in ref path)]
-    (when (nil? @r)
-      (reset! r default))
-    r))
+   :default-key false})
 
 (defn newgame-dialog
-  [{:keys [state* toggle-createdialog]}]
-  (let [visible* (uix/cursor-in state* [:visible])
-        player* (uix/cursor-in state* [:player])
-        playerlabel* (uix/cursor-in state* [:player-label])
-        openclose* (uix/cursor-in state* [:openclose])
-        name (uix/cursor-in state* [:name])
-        close-fn (fn [evt] (reset! visible* false))
-        play-switchval* (uix/state true)
-        inviteonly-state* (defaulting-cursor-in state* [:invite-only] {})]
-    (.log js/console "newgame-dialog state is " @state*)
-    (.log js/console "visible* is " @visible*)
-    (.log js/console "invite-only state is " @inviteonly-state*)
+  [{app-state* :state*}]
+  (let [visible* (uix/cursor-in app-state* [:visible])
+        mystate* (uix/state {:player (:domain (default-switchstate player-config))
+                             :invitation (:domain (default-switchstate invitation-config))})
+        close-fn (fn [evt] (reset! visible* false))]
     [:> dialog/default {:open @visible* :onClose close-fn}
      [:> dialog-title/default {:id :newgame-dialog-title} "Create New Game"]
-     [:> form-group/default {:row true}
-      [:> form-controllabel/default
-       {:label @playerlabel*
-        :control (uix/as-element
-                   [:> switch/default
-                    {:checked @play-switchval*
-                     :name "Player"
-                     :onChange (fn [evt]
-                                 (let [{:keys [player label]}
-                                       (get playerswitch-config js/evt.target.checked)]
-                                   (reset! player* player)
-                                   (reset! playerlabel* label)
-                                   (reset! play-switchval* js/evt.target.checked)))}])}]]
-     [switchgroup {:state* inviteonly-state* :vals-fn inviteonly-statevals}]
-     #_[:> form-controllabel/default
-      {:label "Open"
-       :control (uix/as-element [:> switch/default {:checked true}])}]
+     [:> form-group/default {:row false}]
+     [switchgroup {:title "Play As:"
+                   :vals-fn player-config
+                   :output (uix/cursor-in mystate* [:player])}]
+     [switchgroup {:title "Invite Opponent:"
+                   :vals-fn invitation-config
+                   :output (uix/cursor-in mystate* [:invitation])}]
 
      [:> dialog-content/default nil
       [:> dialog-contenttext/default nil
